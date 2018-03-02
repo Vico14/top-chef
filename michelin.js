0ef1d1f469f3c41aea77e443e8b9f937f81c30e4
Vico14 <32325477+Vico14@users.noreply.github.com>
2018-03-02 16:46:46 +0100

    step2 and modules

--- michelin.js
+++ michelin.js
@@ -1,106 +1,112 @@
-const michelin = require('michelin');
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-console.log(michelin.get());
+
 
 
 
 
 app.get('/scrape', function(req, res){
-    // The URL we will scrape from - in our example Anchorman 2.
+	// The URL we will scrape from - in our example Anchorman 2.
 
-    url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
+	url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
+	url2 = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-'
+	var i=0; 
+	
+	// The structure of our request call
+	// The first parameter is our URL
+	// The callback function takes 3 parameters, an error, response status code and the html
 
-    // The structure of our request call
-    // The first parameter is our URL
-    // The callback function takes 3 parameters, an error, response status code and the html
+	request(url, function(error, response, html){
 
-    request(url, function(error, response, html){
+		// First we'll check to make sure no errors occurred when making the request
 
-        // First we'll check to make sure no errors occurred when making the request
+		if(!error){
+			var $ = cheerio.load(html);
 
-        if(!error){
-            var $ = cheerio.load(html);
+			var title, stars, nbOffres, typeCuisine, price;
+			var json = { title : "", stars : "", nbOffres : "", typeCuisine : "", price : ""};
 
-            var title, stars, nbOffres, typeCuisine, price;
-            var json = { title : "", stars : "", nbOffres : "", typeCuisine : "", price : ""};
-            var $ = cheerio.load(html);
+			$('poi-card-link').each(function(){
 
-
-            $('.poi_card-display-title').filter(function(){
-
-
-                 var data = $(this);
-
-                 title = data.children().first().text();
-
-                 json.title = title;}
-
-             $('.poi_card-display-title').filter(function(){
+				$('.poi_card-display-title').filter(function(){
 
 
-                 var data = $(this);
- 
-                 stars = data.children().first().text();
+					 var data = $(this);
 
-                 json.title = 3;}
+					 title = data.text();
 
-             $('.mtpb2c-offers').filter(function(){
+					 json.title = title;})
 
 
-                 var data = $(this);
 
-                 nbOffres = data.text();
-
-                 json.title = nbOffres;}
+				 $('.mtpb2c-offers').filter(function(){
 
 
-             $('.poi_card-display-cuisines is-truncated').filter(function(){
+					 var data = $(this);
+
+					 nbOffres = data.text();
+
+					 json.title = nbOffres;})
 
 
-                 var data = $(this);
-
-                 typeCuisine = data.text();
-
-                 json.title = typeCuisine;}
+				 $('.poi_card-display-cuisines is-truncated').filter(function(){
 
 
-             $('.poi_card-display-price').filter(function(){
+					 var data = $(this);
+
+					 typeCuisine = data.text();
+
+					 json.title = typeCuisine;})
 
 
-                 var data = $(this);
+				 $('.poi_card-display-price').filter(function(){
 
-                 price = data.text();
 
-                 json.title = price;}
-        }
-    })
+					 var data = $(this);
+
+					 price = data.text();
+
+					 json.title = price;})
+
+
+				 fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+						console.log('File successfully written! - Check your project directory for the output.json file');
+				})
+		})
+	}
+
+		
+
+res.send('Check your console!')
+
+	});
 })
-app.listen('8081')
+
+app.listen('8081');
 console.log('Magic happens on port 8081');
 exports = module.exports = app;
 
 
+//foreach poi-card-link
 
 
-/* 
-nombres étoiles
-guide-icon icon-mr icon-cotation3etoiles
-guide-icon icon-mr icon-cotation2etoiles
-guide-icon icon-mr icon-cotation1etoiles
+//nombres étoiles
+//guide-icon icon-mr icon-cotation3etoiles
+//guide-icon icon-mr icon-cotation2etoiles
+//guide-icon icon-mr icon-cotation1etoiles
 
-nom restau
-poi_card-display-title
+//nom restau
+//poi_card-display-title
 
-nombre offres
-mtpb2c-offers
+//nombre offres
+//mtpb2c-offers
 
-type de cuisine
-poi_card-display-cuisines is-truncated
+//type de cuisine
+//poi_card-display-cuisines is-truncated
 
-fourchette de prix
-poi_card-display-price
+//fourchette de prix
+//poi_card-display-price
