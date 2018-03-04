29b0d617b4abb85714a085fb4bc0d52949a4dd2f
Vico14 <32325477+Vico14@users.noreply.github.com>
2018-03-05 00:01:34 +0100

    michelin better infos

--- michelin.js
+++ michelin.js
@@ -4,53 +4,68 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-
-
-
-
-
 app.get('/scrape', function(req, res){
 
  	var json = {};
  	json['items']=[];
+ 	var url2 = 'https://restaurant.michelin.fr'
 
 	for(var i=0;i<36;i++){
 		url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-'+i;
-		request(url, function(error, response, html){
 
-			// First we'll check to make sure no errors occurred when making the request
+		request(url, function(error, response, html){
 
 			if(!error){
 				var $ = cheerio.load(html);
 
-				$('.poi-card-link').each(function(i,element){
-					var item = { 'title' : '', 'stars' : '', 'nbOffres' : '', 'typeCuisine' : '', 'price' : ''}
+				$('.view-mode-poi_card').each(function(i,element){
+		    		var url_scrapped = $(this).find('.poi-card-link').attr('href');
+		    		var url3= url2+url_scrapped;
 
-					var data = $(this);
+		    		request(url3, function(error, response, html){
 
-					item.title = data.find('.poi_card-display-title').text().trim();
-					item.stars = data.find('.guide').text().trim();
-					if(data.find('.icon-mr').hasClass('icon-cotation1etoile')){
-						item.stars+=' 1 étoile';
-					}
-					if(data.find('.icon-mr').hasClass('icon-cotation2etoiles')){
-						item.stars+=' 2 étoiles';
-					}
-					if(data.find('.icon-mr').hasClass('icon-cotation3etoiles')){
-					item.stars+=' 3 étoiles';
-					}
+						if(!error){
 
-					item.nbOffres = data.find('.mtpb2c-offers').text().trim();
-					item.typeCuisine = data.find('.poi_card-display-cuisines').text().trim();
-					item.price = data.find('.poi_card-display-price').text().trim();
-					
-					json['items'].push(item);
-	
-				})
+							var $ = cheerio.load(html);
+							var item = { 'title' : '','adress': {'street_block':'','postal_code':'','locality':''}, 'stars' : '',
+							'offres': [], 'typeCuisine' : '', 'price' : '','chef':''}
 
-			fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){})
+							item.title = $('.poi_intro-display-title').text().trim();
+							item.stars = $('.guide').text().trim();
+							if($('.guide').children().first().attr('class')=="guide-icon icon-mr icon-cotation1etoile"){
+								item.stars+=' 1 étoile';
+							}
+							if($('.guide').children().first().attr('class')=="guide-icon icon-mr icon-cotation2etoiles"){
+								item.stars+=' 2 étoiles';
+							}
+							if($('.guide').children().first().attr('class')=="guide-icon icon-mr icon-cotation3etoiles"){
+							item.stars+=' 3 étoiles';
+							}
+
+							item.adress.street_block= $('.thoroughfare').first().text().trim();
+							item.adress.postal_code = $('.postal-code').first().text().trim();
+							item.adress.locality = $('.locality').first().text().trim();
+
+							if($('.view-restaurant-offers')[0]){
+					    		$('.view-restaurant-offers').children('.view-content').children().each(function(i,element){
+					    			var info_offres = {'description': '', 'validity': ''};
+					    			info_offres.description= $(this).find('.title-wrapper').children().text().trim();
+					    			info_offres.validity= $(this).find('.validity-dates-wrapper').text().trim();
+					    			item.offres.push(info_offres);
+					    		})
+					    	}
+							item.typeCuisine = $('.poi_intro-display-cuisines').text().trim();
+							item.price = $('.poi_intro-display-prices').text().trim();
+							item.chef = $('.field--name-field-chef').children('.field__items').children().first().text().trim();
+							
+							json['items'].push(item);
+						
+
+							fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){})
+						}
+					})
+				})	
 			}
-
 		});
 	}
 
@@ -60,5 +75,7 @@ app.get('/scrape', function(req, res){
 })
 
 app.listen('8081');
-console.log('Magic happens on port 8081/scrape');
+console.log('goto : localhost:8081/scrape');
 exports = module.exports = app;
+
+//field field--name-field-address field--type-addressfield field--label-hidden opt-upper-var2__address-wrapper
